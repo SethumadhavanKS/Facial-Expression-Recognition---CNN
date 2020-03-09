2672d4cca2a1af73bbdfbc8dd632997ae2113692
SethumadhavanKS <zethumadhavansethu@gmail.com>
2020-03-09 20:51:49 +0530

    Data is splitted into train and test and converted to numpy arrays

---
+++ Model/emotion_recognition.py
@@ -0,0 +1,66 @@
+import sys, os
+import pandas as pd
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization,AveragePooling2D
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.utils import np_utils
+# pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_columns', 500)
+# pd.set_option('display.width', 1000)
+
+df=pd.read_csv('fer2013.csv')
+
+# print(df.info())
+# print(df["Usage"].value_counts())
+
+# print(df.head())
+X_train,train_y,X_test,test_y=[],[],[],[]
+
+for index, row in df.iterrows():
+    val=row['pixels'].split(" ")
+    try:
+        if 'Training' in row['Usage']:
+           X_train.append(np.array(val,'float32'))
+           train_y.append(row['emotion'])
+        elif 'PublicTest' in row['Usage']:
+           X_test.append(np.array(val,'float32'))
+           test_y.append(row['emotion'])
+    except:
+        print("error occured at index :{index} and row:{row}")
+
+
+num_features = 64
+num_labels = 7
+batch_size = 64
+epochs = 30
+width, height = 48, 48
+
+
+X_train = np.array(X_train,'float32')
+train_y = np.array(train_y,'float32')
+X_test = np.array(X_test,'float32')
+test_y = np.array(test_y,'float32')
+
+train_y=np_utils.to_categorical(train_y, num_classes=num_labels)
+test_y=np_utils.to_categorical(test_y, num_classes=num_labels)
+
+#cannot produce
+#normalizing data between oand 1
+X_train -= np.mean(X_train, axis=0)
+X_train /= np.std(X_train, axis=0)
+
+X_test -= np.mean(X_test, axis=0)
+X_test /= np.std(X_test, axis=0)
+
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+
+X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
+
+print("shape:{X_train.shape}")
+##designing the cnn
+#1st convolution layer
