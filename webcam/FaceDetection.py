d0e5ab4e095b09755ca4e855ae633f907ed263fc
SethumadhavanKS <zethumadhavansethu@gmail.com>
2020-05-03 15:26:30 +0530

    Frames converted to video

--- webcam/FaceDetection.py
+++ webcam/FaceDetection.py
@@ -22,8 +22,7 @@ ls=["happy","sad","angry","disgust","neutral","surprise","fear"]
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
-
-#load model
+cap=cv2.VideoCapture(0)#load model
 model = model_from_json(open("fer.json", "r").read())
 #load weights
 model.load_weights('fer.h5')
@@ -31,24 +30,6 @@ model.load_weights('fer.h5')
 ls=["happy","sad","angry","disgust","neutral","surprise","fear"]
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-
-cap=cv2.VideoCapture(0)
-
-while True:
-    ret,test_img=cap.read()# captures frame and returns boolean value and captured image
-    if not ret:
-        continue
-    gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
-
-
-    for (x,y,w,h) in faces_detected:
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,255,0),thickness=2)
-        roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
-        roi_gray=cv2.resize(roi_gray,(48,48))
-        img_pixels = image.img_to_array(roi_gray)
-        img_pixels = np.expand_dims(img_pixels, axis = 0)
 
 cap=cv2.VideoCapture(0)
 
