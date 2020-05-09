9c72438800d9879bc6484a6faf342ea71ff5cb86
SethumadhavanKS <zethumadhavansethu@gmail.com>
2020-05-09 16:27:51 +0530

    Bug in frame without image solved

--- webcam/FaceDetection.py
+++ webcam/FaceDetection.py
@@ -64,48 +64,6 @@ count_angry=ls.count("angry")
 count_disgust=ls.count("disgust")
 count_fear=ls.count("fear")
 count_happy=ls.count("happy")
-cap=cv2.VideoCapture(0)
-
-while True:
-    ret,test_img=cap.read()# captures frame and returns boolean value and captured image
-    if not ret:
-        continue
-    gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
-
-
-    for (x,y,w,h) in faces_detected:
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,255,0),thickness=2)
-        roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
-        roi_gray=cv2.resize(roi_gray,(48,48))
-        img_pixels = image.img_to_array(roi_gray)
-        img_pixels = np.expand_dims(img_pixels, axis = 0)
-        img_pixels /= 255
-
-        predictions = model.predict(img_pixels)
-        #find max indexed array
-        max_index = np.argmax(predictions)
-        
-
-        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-        predicted_emotion = emotions[max_index]
-        ls.append(predicted_emotion)		
-	    
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-
-    resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ',resized_img)
-
-
-
-    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
-        break
-print(ls)
-count_angry=ls.count("angry")
-count_disgust=ls.count("disgust")
-count_fear=ls.count("fear")
-count_happy=ls.count("happy")
 count_sad=ls.count("sad")
 count_surprise=ls.count("surprise")
 count_neutral=ls.count("neutral")
@@ -123,8 +81,11 @@ plt.show()
 
 
 
-print("Angry:",count_angry,"\n","Disgust:",count_disgust,"\n","Fear:",count_fear,"\n",
-"Happy:",count_happy,"\n","Sad:",count_sad,"\n","Surprise:",count_surprise,"\n","Neutral:",count_neutral)
-cap.release()
-cv2.destroyAllWindows
-#
+
+
+
+
+
+
+
+
