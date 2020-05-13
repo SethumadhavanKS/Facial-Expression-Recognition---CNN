6abc8279ba461fd8e52156c2fcd7d5785a5ae4fa
SethumadhavanKS <zethumadhavansethu@gmail.com>
2020-05-13 23:28:48 +0530

    Bug in size of saved images solved

--- webcam/FaceDetection.py
+++ webcam/FaceDetection.py
@@ -81,11 +81,8 @@ plt.show()
 
 
 
-
-
-
-
-
-
-
-
+print("Angry:",count_angry,"\n","Disgust:",count_disgust,"\n","Fear:",count_fear,"\n",
+"Happy:",count_happy,"\n","Sad:",count_sad,"\n","Surprise:",count_surprise,"\n","Neutral:",count_neutral)
+cap.release()
+cv2.destroyAllWindows
+#
